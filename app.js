03ab436f9d3b1494cc76d9530dd61441427b80b6
Dennis <d.oosterling@student.fontys.nl>
2017-04-10 22:25:01 +0200

    Fixed generation of routes Added send to MQ

--- app.js
+++ app.js
@@ -1,29 +1,63 @@
 const coordinates = require('./coords.json');
 const _ = require('lodash');
 const request = require('request');
+const amqp = require('amqplib/callback_api');
 
-const minutes = 0.5;
+const minutes = 0.1;
 const interval = minutes * 60 * 1000;
 
-setInterval(() => {
+let amountOfRoutes = 50;
+let operations = [];
+let myRoutes = [];
+let routeObjectArray = [];
 
-}, interval);
+generateRoutes(amountOfRoutes);
 
-let coords = generateRoutes(10);
+Promise.all(operations).then(() => {
+    setInterval(() => {
+        let removeFromRoutes = [];
 
-_.forEach(coords, (route) => {
-    request.get('http://router.project-osrm.org/route/v1/driving/'
-        + route.beginpoint
-        +';' + route.endpoint
-        + '?overview=false&steps=true', (error, response, body) => {
-            var value = JSON.parse(body);
-
-            let steps = _.map(value['routes'][0]['legs'][0]['steps'], (step) => {
-               return step.maneuver.location;
-            });
-            console.log(steps)
-    });
+        _.forEach(routeObjectArray, (routeObject, index) => {
+            var date = new Date();
+            let waypoint = {};
+            waypoint.cartracker = routeObject.cartrackerId;
+            waypoint.lat = routeObject.route[0][0];
+            waypoint.lon = routeObject.route[0][1];
+            waypoint.date = date;
+            console.log(waypoint);
+            // send message, wait for message to be sent
+            sendToMQ(waypoint);
+            // Check if route has atleast 2 waypoints
+            // Else remove the route from the routes array
+            if(routeObject.route.length >= 2) {
+                routeObject.route.pop();
+            } else {
+                removeFromRoutes.push(index);
+            }
+        });
+        _.forEach(removeFromRoutes, (routeIndex) =>
+        {
+            routeObjectArray.splice(routeIndex, 1);
+            generateRoutes(1);
+        });
+        console.log(routeObjectArray.length)
+    }, interval);
 });
+
+function sendToMQ(mqMessage) {
+    amqp.connect('amqp://localhost', function(err, conn) {
+        conn.createChannel(function(err, ch) {
+            let q = 'hello';
+
+            ch.assertQueue(q, {durable: false});
+            // Note: on Node 6 Buffer.from(msg) should be used
+            ch.sendToQueue(q, new Buffer.from(JSON.stringify(mqMessage)));
+            // ch.sendToQueue(q, new Buffer.from(JSON.stringify(coords[0])));
+            // ch.sendToQueue(q, new Buffer('hi'));
+            console.log(" [x] Sent 'Hello World!'");
+        });
+    });
+}
 
 function generateRoutes(amount) {
 
@@ -49,5 +83,49 @@ function generateRoutes(amount) {
 
         return route;
     });
-    return coordList;
+    getRoutesFromOSRM(coordList);
+    // return coordList;
+}
+
+function getRoutesFromOSRM(coords){
+    coords.forEach((route) => {
+        operations.push(new Promise((resolve, reject) => {
+            request.get('http://router.project-osrm.org/route/v1/driving/'
+                + route.beginpoint
+                +';' + route.endpoint
+                + '?overview=false&steps=true', (error, response, body) => {
+                if(error) {
+                    reject(error);
+                } else {
+                    let value = JSON.parse(body);
+
+                    let steps = _.map(value['routes'][0]['legs'][0]['steps'], (step) => {
+                        return step.maneuver.location;
+                    });
+                    let routeObject = {};
+                    routeObject.cartrackerId = randomIntFromInterval(1, 50);
+                    routeObject.route = steps;
+                    myRoutes.push(steps);
+                    routeObjectArray.push(routeObject);
+                    resolve();
+                }
+            });
+        }));
+    });
+}
+
+function randomIntFromInterval(min,max) {
+    let randomCartrackerId = Math.floor(Math.random()*(max-min+1)+min);
+    while (checkIfCartrackerIdExists(randomCartrackerId)) {
+        randomCartrackerId = Math.floor(Math.random()*(max-min+1)+min);
+    }
+    return randomCartrackerId;
+}
+
+function checkIfCartrackerIdExists(cartrackerId) {
+    let idExists = false;
+    _.forEach(routeObjectArray, (route) => {
+        if (route.cartrackerId === cartrackerId) idExists = true;
+    });
+    return idExists;
 }
